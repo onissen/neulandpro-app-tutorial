a35f115bef86e84609adde288e6a5c616239f156
onissen <56229919+onissen@users.noreply.github.com>
2021-04-23 20:49:47 +0200

    Zweites Video - Scrollbare Liste von Chat Nachrichten

--- app/src/main/java/com/example/whatsapppro/MainActivity.kt
+++ app/src/main/java/com/example/whatsapppro/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -22,36 +24,53 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val chatBubbleData = ChatBubbleData(
-            message = "Hallo",
+        val question = ChatBubbleData(
+            message = "Wie gehts?",
             date = "Gestern",
-            isSender = true,
+            isSender = true
         )
 
-        setContent {
-            Surface(
-                color = if (chatBubbleData.isSender) Color.LightGray else Color.Yellow,
-                shape = RoundedCornerShape(8.dp),
-                elevation = 8.dp,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = 16.dp,
-                        bottom = 16.dp,
-                        start = if (chatBubbleData.isSender) 16.dp else 64.dp,
-                        end = if (chatBubbleData.isSender) 64.dp else 16.dp
-                    )
-            ) {
-                Column (modifier = Modifier.padding(16.dp)) {
+        val answer = ChatBubbleData(
+            message = "Gut",
+            date = "Gestern",
+            isSender = false
+        )
 
-                    Text(text = chatBubbleData.message)
-                    Text(text = chatBubbleData.date,
-                        modifier = Modifier.align(Alignment.End),
-                        style = MaterialTheme.typography.caption
-                    )
+        val chatBubbleList: List<ChatBubbleData> = listOf(question, answer, question, answer, question, answer, question, answer, question, answer, question, answer, question, answer, question, answer,)
+
+        setContent {
+            LazyColumn {
+                items(chatBubbleList) {
+                    ChatBubble(chatBubbleData = it)
                 }
             }
+        }
+    }
 
+    @Composable
+    fun ChatBubble(chatBubbleData: ChatBubbleData) {
+        Surface(
+            color = if (chatBubbleData.isSender) Color.LightGray else Color.Yellow,
+            shape = RoundedCornerShape(8.dp),
+            elevation = 8.dp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 16.dp,
+                    bottom = 16.dp,
+                    start = if (chatBubbleData.isSender) 16.dp else 64.dp,
+                    end = if (chatBubbleData.isSender) 64.dp else 16.dp
+                )
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                Text(text = chatBubbleData.message)
+                Text(
+                    text = chatBubbleData.date,
+                    modifier = Modifier.align(Alignment.End),
+                    style = MaterialTheme.typography.caption
+                )
+            }
         }
     }
 }
